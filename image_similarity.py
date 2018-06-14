e672dbb212824b3e18004f5ecf7012abcd50e4e7
Kevin.Hsiao <kevinisbest26@yahoo.com.tw>
2018-06-15 02:21:34 +0800

    update consine distance

--- image_similarity.py
+++ image_similarity.py
@@ -4,6 +4,8 @@ from keras.applications.vgg19 import preprocess_input, decode_predictions
 import numpy as np
 import os
 import sys
+from scipy.spatial.distance import cosine
+import scipy
 
 image_path = '/Users/kevin_mbp/Desktop/image/'
 output_path = '/Users/kevin_mbp/Desktop/test_dir/output'
@@ -16,14 +18,27 @@ def cosine_similarity(ratings):
         sim = sim.toarray()
     norms = np.array([np.sqrt(np.diagonal(sim))])
     return (sim / norms / norms.T)
-    
+def consine_distance(seed_vector,test_vector):
+    dis = np.zeros(80)
+    for i in range(8):
+        index_max = (i+1)*10-1
+        for j in range(index_max-9,index_max+1,1):
+            print(j)
+            tmp = cosine(seed_vector[j,:],test_vector)
+            dis[j] = tmp
+    dis = dis.reshape(8,10)
+    distance = np.mean(dis,axis=1)
+    # for i in range(a):
+    #     for j in range(a):
+    #         distance[i,j] = cosine(u[i,:],v[j,:])
+    return distance
 def main():
     # 自 images 目錄找出所有 JPEG 檔案    
     y_test=[]
     x_test=[]
     seed_pic=[]
     for class_name in sorted(os.listdir(seed_path)):
-        print('now is ',class_name)
+        print('now is :',class_name)
         for img_path in sorted(os.listdir(seed_path+class_name)):
             if img_path.endswith('.jpg'):
                 img = image.load_img(seed_path+class_name+'/'+img_path, target_size=(224, 224))
@@ -58,19 +73,28 @@ def main():
     # 萃取特徵
     features_seed = model.predict(seed_pic)
     features_test = model.predict(x_test)
-    print(features_test.shape)
-    print(features_seed.shape)
-    features_total = np.concatenate((features_test,features_seed))
-    print(features_total.shape)
+    # print(features_test.shape)
+    # print(features_seed.shape)
+    # features_total = np.concatenate((features_test,features_seed))
+    # print(features_total.shape)
     # 計算相似矩陣
-    features_compress = features_test.reshape(len(seed_pic),7*7*512)
-    sim = cosine_similarity(features_compress)
-    print(sim)
+    
+    features_compress_seed = features_seed.reshape(len(seed_pic),7*7*512)
+    features_compress_test = features_test.reshape(len(y_test),7*7*512)
+
+    # sim = cosine_similarity(features_compress)
+    for i in range(len(y_test)):
+        distance = consine_distance(features_compress_seed,features_compress_test[i,:])
+        print(distance)
+    # print(sim)
+    # print(distance)
     # 依命令行參數，取1個樣本測試測試
-    inputNo = 1 # tiger, np.random.randint(0,len(y_test),1)[0]
-    top = np.argsort(-sim, axis=1)[:,0:5]
-    print(top)
-    print(len(x_test),len(y_test))
+    # inputNo = 1 # tiger, np.random.randint(0,len(y_test),1)[0]
+    # top = np.argsort(-sim, axis=1)[:,0:5]
+    # print(top)
+    # top = np.argsort(distance, axis=1)[:,0:5]
+    # print(top)
+    # print(len(x_test),len(y_test))
     # 取得最相似的前3名序號
     # recommend = [y_test[i] for i in top]
     # print(recommend)
