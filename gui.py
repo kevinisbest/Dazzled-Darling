fbbada55c0f27a115ee53ed7d450422b7e3c14db
Kevin.Hsiao <kevinisbest26@yahoo.com.tw>
2018-06-22 15:47:18 +0800

    add count
    still some bugs

--- gui.py
+++ gui.py
@@ -1,33 +1,36 @@
 from tkinter import *
-import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import ttk
 from PIL import ImageTk
 from PIL import Image
 import os
 from os import listdir
 import random
 import glob
+win=Tk()
+win.geometry('1000x600')
+win.resizable(0,0)
+win.title("IR ttk GUI")
+label=Label(win, text="Hello World!")
+count=0
 
-
-def helloCallBack():
-   messagebox.showinfo( "Hello Python", "Hello World")
- 
 def show():
+    global count
+    count = count + 1
     panelA = None
     panelB = None
     srcw = 450
     srch = 450
     image_list = []
     images = glob.glob('/Users/kevin_mbp/Desktop/IG/image/*.jpg')
+    label.config(text = 'count '+str(count))
+    
+    picA = random.sample(images, 1)[0]
+    image_list.append(picA)
+    picB = random.sample(images, 1)[0]
+    image_list.append(picB)
 
-    for i in range(3):
-        picA = random.sample(images, 1)[0]
-        image_list.append(picA)
-        picB = random.sample(images, 1)[0]
-        image_list.append(picB)
-
-        picA = Image.open(picA)
-        picB = Image.open(picB)
+    picA = Image.open(picA)
+    picB = Image.open(picB)
     
     # print('image size:',edged.size)
     picA = picA.resize( (srcw, srch), Image.BILINEAR )
@@ -39,12 +42,12 @@ def show():
     if panelA is None or panelB is None:
         panelA = Label(image = picA,text = 'out')
         panelA.image = picA
-        panelA.pack(side = "right", padx=10, pady=10)
+        panelA.pack(side = "left", )
         
         # while the second panel will store the edge map
         panelB = Label(image = picB,text = 'out')
         panelB.image = picB
-        panelB.pack(side = "right", padx=10, pady=10)
+        panelB.pack(side = "right")
         
     # otherwise, update the image panels
     else:
@@ -54,13 +57,7 @@ def show():
         panelB.configure(image = picB)
         panelB.image = picB
 
-def main():
-    window = tk.Tk()
-    window.title("IR")
-    window.geometry('1080x720')
-    btn_select_image = Button(window, text="hit me",activeforeground = 'red', command = show)
-    btn_select_image.pack(side = "top", fill = "both", expand = "yes", padx = "10", pady = "10")
-    window.mainloop()
-
-if __name__ == '__main__':
-    main()
+button=Button(win, text="OK",command=show)
+label.pack()
+button.pack()
+win.mainloop()
