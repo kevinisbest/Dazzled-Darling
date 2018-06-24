4305202d9114db43474473fc0ac9aed51b57e36c
Kevin.Hsiao <kevinisbest26@yahoo.com.tw>
2018-06-24 19:49:54 +0800

    update gui

--- gui.py
+++ gui.py
@@ -12,7 +12,7 @@ win.geometry('800x600')
 win.title("迷惘美")
 win.configure(background='gray')
 
-count = 0
+count = -1
 srcw = 450
 srch = 450
 
@@ -43,29 +43,56 @@ class Test():
         label = Label(win, text="Welcom to 迷惘美, please select Like or Dislike")
         label.pack()
 
+        global r1, r2
+
         r1 = Radiobutton(win, text='Like',
-                    variable=var, value='Yes',
+                    variable=var, value='Like',
                     command=print_selection)
         r1.pack()
         r2 = Radiobutton(win, text='Dislike',
-                            variable=var, value='NO',
+                            variable=var, value='Dislike',
                             command=print_selection)
         r2.pack()
 
     def changeImg(self):
-        self.picB = random.sample(images, 1)[0]
-        image_list.append(self.picB)
-        self.picB = Image.open(self.picB)
-        # self.picB = self.picB.resize( (srcw, srch), Image.BILINEAR )
-        s = self.picB.size
-        print(s)
-        ratio = 500/max(s[0],s[1])
-        print(ratio)
-        self.picB.thumbnail((int(s[0]*ratio),int(s[1]* ratio)),Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(self.picB)
-        self.canvas.itemconfig(self.imgArea, image = self.img)
-        self.but2 = Button(win, text="press me", command=lambda: self.changeImg())
+        global count
+        count+=1
+        if count ==7:
+            self.canvas.destroy()
+            self.but1.destroy()
+            r1.destroy()
+            r2.destroy()
+            self.text()
+        else:
+            self.picB = random.sample(images, 1)[0]
+            image_list.append(self.picB)
+            self.picB = Image.open(self.picB)
+            # self.picB = self.picB.resize( (srcw, srch), Image.BILINEAR )
+            s = self.picB.size
+            ratio = 500/max(s[0],s[1])
+            print(count)
+            self.picB.thumbnail((int(s[0]*ratio),int(s[1]* ratio)),Image.ANTIALIAS)
+            self.img = ImageTk.PhotoImage(self.picB)
+            self.canvas.itemconfig(self.imgArea, image = self.img)
+            self.but2 = Button(win, text="Next !", command=lambda: self.changeImg())
+            self.but2.place(x=10, y=500)
+    def text(self):
+        print('del')
+
+        
+        label = Label(win, text="輸入關鍵字：")
+        label.pack(side='top')
+        xls_text = StringVar()
+        xls = Entry(win, textvariable = xls_text)
+        xls_text.set(" ")
+        xls.pack()
+        self.but2 = Button(win, text="ok !", command=self.do_somthing())
         self.but2.place(x=10, y=500)
+        self.but2.pack()
+
+
+    def do_somthing(self):
+        print('do_somthing')
 
 
 
@@ -121,6 +148,7 @@ def update_image():
     label.after(1000, update_image)
     print ("Updated")
 def print_selection():
+
     label.config(text='you have selected ' + var.get())
 
 
