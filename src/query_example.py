8c82bf38c8593f66426587d6f8509999c0780236
Shanboy5566 <b01204037@ntu.edu.tw>
2018-06-26 23:42:20 +0800

    remove arg

--- src/query_example.py
+++ src/query_example.py
@@ -6,12 +6,11 @@ import sys
 Prepare: 將query.py放在./下
 Work:
 import query
-import sys
-argv[1]放我們的Database (e.g. output_new_policy3.txt) [Type: txt]
+input_dataset = Database的路徑 (e.g. ./output_new_policy3.txt) [Type: txt]
 prefer_score = user-prefer的分數 (老蕭前端) [Type: dic]
 query_list = Text model產出的結果 (振合&阿岳) [Type: list]
 
-1. call query.buildDataBase(sys.argv[1]) 來連結Database
+1. call query.buildDataBase(input_dataset) 來連結Database
     return userList[Type: list], Database[Type: list]
 2. call query.comparePreAndQry(prefer_score, query_list, userList, Database) 來比對user_prefer與text model的結果，並由關聯度大到小排序
     return query_list[Type: list]
