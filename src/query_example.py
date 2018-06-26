7f97459077268625f7395533a5f4d3ac473b1da9
Shanboy5566 <b01204037@ntu.edu.tw>
2018-06-26 23:39:48 +0800

    fix arg

--- src/query_example.py
+++ src/query_example.py
@@ -8,12 +8,12 @@ Work:
 import query
 import sys
 argv[1]放我們的Database (e.g. output_new_policy3.txt) [Type: txt]
-argv[2]放user-prefer的分數 (老蕭前端) [Type: dic]
-argv[3]放Text model產出的結果 (振合&阿岳) [Type: list]
+prefer_score = user-prefer的分數 (老蕭前端) [Type: dic]
+query_list = Text model產出的結果 (振合&阿岳) [Type: list]
 
 1. call query.buildDataBase(sys.argv[1]) 來連結Database
     return userList[Type: list], Database[Type: list]
-2. call query.comparePreAndQry(sys.argv[2], sys.argv[3], userList, Database) 來比對user_prefer與text model的結果，並由關聯度大到小排序
+2. call query.comparePreAndQry(prefer_score, query_list, userList, Database) 來比對user_prefer與text model的結果，並由關聯度大到小排序
     return query_list[Type: list]
 '''
 
